ee6ce59a83e7c63686db35df5ead98252eb32b91
현태수 <qvil@op.gg>
2019-09-28 11:19:30 -0700

    Modify options object

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,14 @@ import { GraphQLServer } from "graphql-yoga";
 import typeDefs from "../graphql/typeDefs";
 import resolvers from "../graphql/resolvers";
 
-const PORT = 4000;
-const endpoint = "/graphql";
-const playground = "/playground";
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start({ endpoint, playground }, () => {
+const options = {
+  port: 4000,
+  endpoint: "/graphql",
+  playground: "/playground"
+};
+
+server.start(options, () => {
   console.log(`Server playground: http://localhost:${PORT}${playground}`);
   console.log(`Server endpoint: http://localhost:${PORT}${endpoint}`);
 });
